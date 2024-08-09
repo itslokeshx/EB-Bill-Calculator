eab75737d5a35fe5e24f4bfa3e15697100a1b2bd
LOKESH A <131357693+369loki@users.noreply.github.com>
2024-08-09 11:23:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
  
 document.querySelector(".final").style.display="none";
-/*alert("Please fill  the values before clicking the calculate button.")*/
+alert("(Revised on July 01, 2024)");
 
- function hari(){
-
+ function display(){
+    
     var c =  document.querySelector("#place").value;
     var b =  document.querySelector("#no").value;
     var a =  document.querySelector("#bro").value;
@@ -25,53 +25,49 @@ document.querySelector(".john1").style.padding="1px";
         document.getElementById("hope").innerHTML=" ₹0.00"}
 
         else if (a>100 && a<=200){
-            joker=((a-100)*2.25)
+            joker=((a-100)*2.35)
             document.getElementById("hope").innerHTML=" ₹"+joker }
 
          else if (a>200 && a<=400){
             var joker,charge;
             joker =( a - 200);
-            charge = (100 * 2.25);
-            charge += (joker * 4.50);
+            charge = (100 * 2.35);
+            charge += (joker * 4.70);
             document.getElementById("hope").innerHTML=" ₹"+charge}
            
             else if (a>400 && a<=500){
                 var joker,charge;
                 joker =( a - 400);
-                charge = (100*2.25)+(200*4.50);
-                charge += (joker *6.00);
+                charge = (100*2.35)+(200*4.70);
+                charge += (joker *6.30);
                 document.getElementById("hope").innerHTML=" ₹"+charge}
         
             else if (a>500 && a<=600){
                 var joker,charge;
                 joker=(a-500);
-                charge = (300*4.50)+(100*6.00);
-                charge += (joker*8.00);
+                charge = (300*4.70)+(100*6.30);
+                charge += (joker*8.40);
                 document.getElementById("hope").innerHTML=" ₹"+charge }
 
             else if(a>600 && a<=800){
                 var joker,charge;
             joker=a-600
-            charge=(300*4.50)+(100*6.00)+(100*8.00)
-            charge+=(joker*9.00)
+            charge=(300*4.70)+(100*6.30)+(100*8.40)
+            charge+=(joker*9.45)
             document.getElementById("hope").innerHTML=" ₹"+charge }
 
             else if(a>800 && a<=1000){
                 var joker,charge;
                 joker=a-800
-            charge=(300*4.50)+(100*6.00)+(100*8.00)+(200*9.0)
-            charge+=(joker*10.00)
+            charge=(300*4.70)+(100*6.30)+(100*8.40)+(200*9.45)
+            charge+=(joker*10.50)
             document.getElementById("hope").innerHTML=" ₹"+charge  }
 
             else{
                 var joker,charge;
                 joker=a-1000
-            charge=(300*4.50)+(100*6.00)+(100*8.00)+(200*9.0)+(200*10.00)
-            charge+=(joker*11.00)
-            document.getElementById("hope").innerHTML=" ₹"+charge  }
-            
-                
+            charge=(300*4.70)+(100*6.30)+(100*8.40)+(200*9.45)+(200*10.50)
+            charge+=(joker*11.55)
+            document.getElementById("hope").innerHTML=" ₹"+charge  }               
 }
-
-
 
